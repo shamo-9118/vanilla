c052e3aedee1bed4829eaf4e55ccdaae0896ec40
shamo-9118 <won9118391183@gmail.com>
2022-11-15 07:32:45 +0900

    learn type guard

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 "use strict";
-const text = "foo";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const userData = {
+    name: "marumoto",
+    height: 162,
+};
+const foo = (value) => {
+    if (!value) {
+        console.log("null");
+        return;
+    }
+    console.log("true");
+    return;
+};
+foo("true");
+const isUserA = (user) => {
+    return user.lang === "ja";
+};
+const isUserB = (user) => {
+    return user.lang === "en";
+};
+const Users = (users) => {
+    if (isUserA(users)) {
+        console.log(users);
+        return;
+    }
+    if (isUserB(users)) {
+        console.log(users);
+        return;
+    }
+};
+const fonn = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch("");
+    const json = yield res.json();
+    if (isUserA(json)) {
+        return json;
+    }
+    if (isUserB(json)) {
+        return json;
+    }
+});
+const persons = [
+    { name: "田中", lang: "ja" },
+    { name: "中田", lang: "ja" },
+    { name: "mark", lang: "en" },
+];
+const japanese = persons.filter((person) => person.lang === "ja");
+const english = persons.filter(isUserB);
+console.log(japanese);
+console.log(english);
--- index.ts
+++ index.ts
@@ -1,1 +1,80 @@
-const text:string ="foo"
+import { getConstantValue } from "typescript";
+
+type User = {
+    name: "marumoto";
+} & OptionalPersonalData;
+
+type PersonalData = {
+    height: 162;
+    weight: 72;
+};
+
+type OptionalPersonalData = {
+    [K in keyof PersonalData]?: PersonalData[K];
+};
+
+const userData: User = {
+    name: "marumoto",
+    height: 162,
+};
+
+const foo = (value: string | null) => {
+    if (!value) {
+        console.log("null");
+        return;
+    }
+    console.log("true");
+    return;
+};
+foo("true");
+
+//ユーザー定義のType Guard
+//しようする値の方がわからない時に使うことがある
+
+type UserA = { name: string; lang: "ja" };
+type UserB = { name: string; lang: "en" };
+
+const isUserA = (user: UserA | UserB): user is UserA => {
+    return user.lang === "ja";
+};
+const isUserB = (user: UserA | UserB): user is UserB => {
+    return user.lang === "en";
+};
+
+const Users = (users: any) => {
+    if (isUserA(users)) {
+        console.log(users);
+        return;
+    }
+    if (isUserB(users)) {
+        console.log(users);
+        return;
+    }
+};
+
+const fonn = async () => {
+    const res = await fetch("");
+    const json = await res.json();
+    if (isUserA(json)) {
+        return json;
+    }
+    if (isUserB(json)) {
+        return json;
+    }
+};
+
+const persons: (UserA | UserB)[] = [
+    { name: "田中", lang: "ja" },
+    { name: "中田", lang: "ja" },
+    { name: "mark", lang: "en" },
+];
+type personA = { name: string; lang: "ja" };
+type personB = { name: string; lang: "en" };
+
+const japanese = persons.filter(
+    (person): person is personA => person.lang === "ja"
+);
+const english = persons.filter(isUserB); //書き方はどちらでもいい！
+
+console.log(japanese);
+console.log(english);
